d4839c48d9c637975a3eabe31c69245fc3e97059
church89 <l.chierici89@gmail.com>
2022-02-23 11:45:11 +0000

    add examples folder

--- examples/msre_ex1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#run with local material data definition
-simrun --h5mfilename msre.h5m -p 1000000 -b 100 --coredim -75 -75 0 75 75 250 --plotgeom --localdata mat_msre --meshscores flux absorption fission
--- examples/msre_ex2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#run with default msre material data library
-simrun --h5mfilename msre.h5m -p 1000000 -b 100 --coredim -75 -75 0 75 75 250 --plotgeom --materialdata msre --meshscores flux absorption fission
